f69c9f8ed561d119fa74e4a7403b8b8d9cf48c9f
凯歌 吕 <2480762082@qq.com>
2023-06-02 21:17:21 +0800

    页面跳转完成

--- hello_myfirend/urls.py
+++ hello_myfirend/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 
 from . import views
 
-app_name = "hello_myfriend"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("hello/", views.hello,),
 ]
--- hello_myfirend/views.py
+++ hello_myfirend/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 
 from django import forms
 
+
 def index(request):
-    return render(request, "friends/index.html")
+    return render(request, "friends/test.html")
+
+
+def hello(request):
+    return render(request, "friends/hello.html")
+
 
