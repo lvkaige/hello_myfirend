cad685b786568b4ececb3865ee5beaa21addb4e2
凯歌 吕 <2480762082@qq.com>
2023-06-02 22:21:33 +0800

    finished

--- hello_myfirend/urls.py
+++ hello_myfirend/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("hello/", views.hello,),
+    path("hello/", views.hello),
+    path("hello/hello2/", views.hello2),
+    path("hello/hello2/hello3/", views.hello3),
+    path("hello/hello2/hello3/hello4/", views.hello4),
 ]
--- hello_myfirend/views.py
+++ hello_myfirend/views.py
@@ -6,10 +6,19 @@ from django import forms
 
 
 def index(request):
-    return render(request, "friends/test.html")
+    return render(request, "friends/index.html")
 
 
 def hello(request):
     return render(request, "friends/hello.html")
+
+def hello2(request):
+    return render(request, "friends/hello2.html")
+
+def hello3(request):
+    return render(request, "friends/hello3.html")
+
+def hello4(request):
+    return render(request, "friends/hello4.html")
 
 
